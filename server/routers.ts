8de388f526b5fc80eb746d27cec95b5ad15fef4d
I-am-Fridgee <iamfridgee@gmail.com>
2025-12-04 01:51:20 -0700

    Update routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -1,12 +1,12 @@
+import { leaderboardRouter } from "./leaderboard";
 import { COOKIE_NAME } from "@shared/const";
 import { getSessionCookieOptions } from "./_core/cookies";
 import { systemRouter } from "./_core/systemRouter";
 import { protectedProcedure, publicProcedure, router } from "./_core/trpc";
-import { getPlayerRank, getTopPlayers, updatePlayerScore } from "./db";
 import { z } from "zod";
 
 export const appRouter = router({
-    // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
+  leaderboard: leaderboardRouter,  // ← NEW leaderboard (has all features)
   system: systemRouter,
   auth: router({
     me: publicProcedure.query(opts => opts.ctx.user),
@@ -18,23 +18,8 @@ export const appRouter = router({
       } as const;
     }),
   }),
-
-  leaderboard: router({
-    getTop: publicProcedure.query(async () => {
-      return await getTopPlayers(10);
-    }),
-    submitScore: protectedProcedure
-      .input(z.object({ score: z.number().int().min(0) }))
-      .mutation(async ({ ctx, input }) => {
-        const username = ctx.user.name || ctx.user.email || "Anonymous";
-        await updatePlayerScore(ctx.user.id, username, input.score);
-        const rank = await getPlayerRank(ctx.user.id);
-        return { success: true, rank };
-      }),
-    getMyRank: protectedProcedure.query(async ({ ctx }) => {
-      return await getPlayerRank(ctx.user.id);
-    }),
-  }),
+  // Remove the old leaderboard: router({ ... }) section
 });
+
 
 export type AppRouter = typeof appRouter;
