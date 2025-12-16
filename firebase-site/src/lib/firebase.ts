b1016aa92663a7f267fcd18973198bf869cff853
I-am-Fridgee <iamfridgee@gmail.com>
2025-12-15 23:06:40 -0700

    Update firebase.ts

--- firebase-site/src/lib/firebase.ts
+++ firebase-site/src/lib/firebase.ts
@@ -1,8 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'; // ADD THIS
+import { getFirestore } from 'firebase/firestore'; // ADD THIS LINE
 
-// Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -12,9 +11,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize services
 export const auth = getAuth(app);
-export const db = getFirestore(app); //ADD this for leaderboard
+export const db = getFirestore(app); // ADD THIS LINE
